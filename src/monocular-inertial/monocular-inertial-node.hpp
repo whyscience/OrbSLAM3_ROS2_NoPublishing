ffe718ee2ff2ace364e4365b0eb73e43592337c1
TheRealBeef <84393940+TheRealBeef@users.noreply.github.com>
2024-05-02 20:22:10 +0200

    Some more changes...

--- src/monocular-inertial/monocular-inertial-node.hpp
+++ src/monocular-inertial/monocular-inertial-node.hpp
@@ -26,6 +26,7 @@ public:
 private:
     void GrabImu(const ImuMsg::SharedPtr msg);
     void GrabImage(const sensor_msgs::msg::Image::SharedPtr msg);
+    cv::Mat GetImage(const ImageMsg::SharedPtr msg);
     void SyncWithImu();
 
     ORB_SLAM3::System* m_SLAM;
@@ -37,6 +38,9 @@ private:
     std::queue<ImuMsg::SharedPtr> imuBuf_;
     std::queue<ImageMsg::SharedPtr> imgBuf_;
     std::mutex bufMutex_;
+
+    bool bClahe_;
+    cv::Ptr<cv::CLAHE> clahe_ = cv::createCLAHE(3.0, cv::Size(8, 8));
 
     std::thread *syncThread_;
 };
